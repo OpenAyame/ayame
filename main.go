70107752892dd8fd4897e7d3d63c62c32b26525f
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2019-02-28 23:37:26 +0900

    ./ayame version でバージョンを表示& ./ayame 起動時にデバッグログを表示するようにする

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-var addr = flag.String("addr", "localhost:3000", "http service address")
+var addr = flag.String("addr", "localhost:3000", " http service address")
+var AyameVersion = "19.02.1"
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+	// 引数の処理
+	if len(args) > 0 {
+		if args[0] == "version" {
+			fmt.Printf("WebRTC Signaling Server Ayame version %s", AyameVersion)
+			return
+		}
+	}
+	log.SetFlags(0)
+	log.Printf("Ayame Signaling Server\n version %s\n running on http://%s (Press Ctrl+C quit)\n", AyameVersion, *addr)
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +30,5 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		wsHandler(hub, w, r)
 	})
-	flag.Parse()
-	log.SetFlags(0)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
